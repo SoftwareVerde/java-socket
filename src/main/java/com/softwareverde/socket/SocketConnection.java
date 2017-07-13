7bcd0e0916d513dd1113a21f264bbc5836280522
Josh Green <josh@softwareverde.com>
2017-07-13 08:22:20 -0400

    Refactored read-thread to properly detect disconnect.

--- src/main/java/com/softwareverde/socket/SocketConnection.java
+++ src/main/java/com/softwareverde/socket/SocketConnection.java
@@ -2,33 +2,27 @@ package com.softwareverde.socket;
 
 import com.softwareverde.async.HaltableThread;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class SocketConnection {
     private static final Object _nextIdMutex = new Object();
     private static Long _nextId = 0L;
-
-    public static class ClosedSocketException extends RuntimeException {
-        public ClosedSocketException() {
-            super("Attempted to access the socket after it has been closed via SocketConnection.close().");
-        }
-    }
 
     private final Long _id;
     private final Socket _socket;
     private PrintWriter _out;
     private BufferedReader _in;
     private final List<String> _messages = new ArrayList<String>();
-    private Boolean _isClosed = false;
+    private volatile Boolean _isClosed = false;
 
     private Runnable _messageReceivedCallback;
-    private HaltableThread _readThread;
+    private Thread _readThread;
+
+    private InputStream _rawInputStream;
 
     private void _executeMessageReceivedCallback() {
         if (_messageReceivedCallback != null) {
@@ -37,11 +31,20 @@ public class SocketConnection {
     }
 
     /**
-     * Internal callback that executed when a message is received by the client.
+     * Internal callback that is executed when a message is received by the client.
      *  Is executed before any external callbacks are received.
-     *  Primarily created for subclass extension.
+     *  Intended for subclass extension.
      */
     protected void _onMessageReceived(final String message) {
+        // Nothing.
+    }
+
+    /**
+     * Internal callback that is executed when the connection is closed by either the client or server,
+     *  or if the connection is terminated.
+     *  Intended for subclass extension.
+     */
+    protected void _onSocketClosed() {
         // Nothing.
     }
 
@@ -52,17 +55,23 @@ public class SocketConnection {
         }
 
         _socket = socket;
+
         try {
             _out = new PrintWriter(socket.getOutputStream(), true);
-            _in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            _rawInputStream = socket.getInputStream();
+            _in = new BufferedReader(new InputStreamReader(_rawInputStream));
         } catch (final IOException e) { }
 
-        _readThread = new HaltableThread(new HaltableThread.ShouldContinueRunnable() {
+        _readThread = new Thread(new Runnable() {
             @Override
-            public Boolean run() {
-                try {
-                    if (_in.ready()) {
+            public void run() {
+                while (! _isClosed) {
+                    try {
                         final String message = _in.readLine();
+                        if (message == null) {
+                            throw new IOException("Remote socket closed the connection.");
+                        }
 
                         synchronized (_messages) {
                             _messages.add(message);
@@ -71,16 +80,13 @@ public class SocketConnection {
                             _executeMessageReceivedCallback();
                         }
                     }
+                    catch (final IOException exception) {
+                        _isClosed = true;
+                        _onSocketClosed();
+                    }
                 }
-                catch (final IOException e) {
-                    e.printStackTrace();
-                    return false;
-                }
-
-                return true;
             }
         });
-        _readThread.setSleepTime(200L);
 
         _readThread.start();
     }
@@ -90,8 +96,6 @@ public class SocketConnection {
     }
 
     synchronized public void write(final String outboundMessage) {
-        if (_isClosed) { throw new ClosedSocketException(); }
-
         _out.write(outboundMessage.trim() + "\n");
         _out.flush();
     }
@@ -113,20 +117,33 @@ public class SocketConnection {
      *  Invoking any write functions after this call throws a runtime exception.
      */
     public void close() {
-        _readThread.halt();
+        _isClosed = true;
+
+        try {
+            _rawInputStream.close();
+        } catch (final Exception exception) { }
+
+        try {
+            _readThread.join();
+        } catch (final InterruptedException e) { }
 
         try {
             _socket.close();
         }
         catch (final IOException e) { }
 
-        _isClosed = true;
+        _onSocketClosed();
     }
 
     /**
      * Returns false if this instance has had its close() function invoked or the socket is no longer connected.
      */
     public Boolean isConnected() {
+        if (_out.checkError()) {
+            _isClosed = true;
+            _onSocketClosed();
+        }
+
         return (! (_isClosed || _socket.isClosed()));
     }
 
